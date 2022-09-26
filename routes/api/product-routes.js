57108edad40eac196cf10782825abf7d7430240d
alkasah <alkasah485@gmail.com>
2022-09-25 17:14:09 -0700

    saving modification

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+// router.post('/', (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -31,26 +31,36 @@ router.post('/', (req, res) => {
     }
   */
  
-  Product.create(req.body)
-    .then((product) => {
+  // Product.create(req.body)
+  //   .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+    //   if (req.body.tagIds.length) {
+    //     const productTagIdArr = req.body.tagIds.map((tag_id) => {
+    //       return {
+    //         product_id: product.id,
+    //         tag_id,
+    //       };
+    //     });
+    //     return ProductTag.bulkCreate(productTagIdArr);
+    //   }
+    //   // if no product tags, just respond
+    //   res.status(200).json(product);
+    // })
+    // .then((productTagIds) => res.status(200).json(productTagIds))
+    // .catch((err) => {
+    //   console.log(err);
+    //   res.status(400).json(err);
+    // });
+//});
+
+router.post('/',(req,res)=>{
+  Product.create(req.body)
+  .then((newProduct) => {
+    res.json(newProduct)
+  })
+  .catch((err)=>{
+    res.json(err);
+  });
 });
 
 // update product
